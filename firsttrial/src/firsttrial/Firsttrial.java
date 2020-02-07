bf635ab64690cd25d4d39000332f5e9e8cb62fd8
Edward <Edward@10.81.3.209>
2020-02-07 15:54:03 -0500

    I added my name

--- firsttrial/src/firsttrial/Firsttrial.java
+++ firsttrial/src/firsttrial/Firsttrial.java
@@ -16,6 +16,10 @@ public class Firsttrial {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        myName();
+    }
+    public static void myName(){
+        System.out.println("My name is Ed");
     }
     
 }
